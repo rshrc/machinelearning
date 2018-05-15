8d46085b075079b5bb71698818aad29e5ae2947a
Rishi Banerjee <rishieric91@gmail.com>
2018-05-15 16:47:21 +0700

    new algorithms added

--- Natural Language Processing/natural_language_processing.py
+++ Natural Language Processing/natural_language_processing.py
@@ -24,5 +24,28 @@ for i in range(0,1000):
     
 # Creating the Bag of Words Model
 from sklearn.feature_extraction.text import CountVectorizer
-cv = CountVectorizer()
+cv = CountVectorizer(max_features = 1500)
 X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:,1].values
+
+# splitting the test data and the training data
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
+
+# Fitting the classifier to the training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the confusion Matrix
+from sklearn.metrics import confusion_matrix  # it is used to see how many wrong and how many correct
+cm = confusion_matrix(y_test, y_pred)
+
